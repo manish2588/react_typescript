63d75c9ec1561d13d0362c3ff8691624e8b8018c
manish2588 <manishkc258@gmail.com>
2025-01-24 18:09:23 +0545

    Added

--- src/App.tsx
+++ src/App.tsx
@@ -4,38 +4,40 @@ import Props from "./components/Props";
 
 
 function App() {
-  const personAddress={
-    town:"Kathmandu",
-    country:"Nepal"
-  }
-   const showMessage=()=>{
-    alert("Hello User")
-   }
-   const showEventType = (event: React.MouseEvent<HTMLButtonElement>) => {
+  const personAddress = {
+    town: "Kathmandu",
+    country: "Nepal",
+  };
+  const showMessage = () => {
+    alert("Hello User");
+  };
+  const showEventType = (event: React.MouseEvent<HTMLButtonElement>) => {
     alert(event.type); // Showing event type
   };
-  const personList=[
+  const personList = [
     {
-      name:"Manish",
-      class:"Bachelor"
+      name: "Manish",
+      class: "Bachelor",
     },
     {
-      name:"Manish",
-      class:"Bachelor"
+      name: "Manish",
+      class: "Bachelor",
     },
     {
-      name:"Manish",
-      class:"Bachelor"
-    }
-  ]
+      name: "Manish",
+      class: "Bachelor",
+    },
+  ];
   return (
     <div>
-      <Props name="Ram" age={15} obj={personAddress}/>
-      <PersonList list={personList}/>
-      <br/>
-      <EventProps handleClick={showMessage} handleClick2={showEventType}/>
+      <Props name="Ram" age={15} obj={personAddress} />
+      <PersonList list={personList} />
+      <br />
+
+      <EventProps handleClick={showMessage} handleClick2={showEventType} />
+      
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ src/components/StyleProps.tsx
@@ -0,0 +1,10 @@
+
+
+function StyleProps() {
+  return (
+    <div>StyleProps</div>
+  )
+}
+
+export default StyleProps;
+// style types are predefined in react
